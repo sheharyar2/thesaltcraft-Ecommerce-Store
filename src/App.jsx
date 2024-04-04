bb5cb77ced4b636fcd2bbca721ac769e0a0f7f6b
Sheharyar Javed <sheharyarj@gmail.com>
2024-04-04 20:48:11 +0500

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import Contact from "./Pages/Contact/Contact";
+import Home from "./Pages/Home/Home";
+import Shop from "./Pages/Shop/Shop";
+import Navbar from "./components/Navbar/Navbar"
+import About from "./Pages/About/About";
+import Blog from "./Pages/Blog/Blog";
+import {BrowserRouter,Routes,Route} from 'react-router-dom';
+
+function App() {
+ 
+  return (
+   <div>
+    <BrowserRouter>
+    <Navbar />
+    <Routes>
+      <Route path="/" element={<Home/>}/>
+      <Route path="/shop" element={<Shop />}/>
+      <Route path="/about" element={<About />}/>
+      <Route path="/contact" element={<Contact />}/>
+      <Route path="/blog" element={<Blog />}/>
+    </Routes>
+    </BrowserRouter>
+   </div>
+  )
+}
+
+export default App
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,40 @@
+import { Link } from "react-router-dom";
+import styles from './Navbar.module.css'
+import logo_black from '../../assets/logo_black.png'
+import heart from '../../assets/icons/heart.png'
+import cart from '../../assets/icons/cart.png'
+
+
+function Navbar() {
+    return (
+        <div className={styles.navbar}>
+            <ul className={styles.nav_menu}>
+                <li>
+                    <Link to="/">Home</Link>
+                </li>
+                <li>
+                    <Link to="/shop">Shop</Link>
+                </li>
+                <li>
+                    <Link to="/blog">Blog</Link>
+                </li>
+                <li>
+                    <Link to="/about">About us</Link>
+                </li>
+                <li>
+                    <Link to="/contact">Contact us</Link>
+                </li>
+            </ul>
+            <div className={styles.nav_logo}>
+            <img src={logo_black} alt="" />
+            </div>
+            <div className={styles.nav_right}>
+                <Link to="/login">
+                    <img src={heart} alt="" className={styles.nav_wishlist}/>
+                </Link>
+                <img src={cart} className={styles.nav_cart} alt="" />
+            </div>
+        </div>
+    );
+}
+export default Navbar;
